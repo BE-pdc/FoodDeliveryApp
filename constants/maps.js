181f4eba154d2878ce818da89fc2f50d00f5c8b7
BE-pdc <80090300+BE-pdc@users.noreply.github.com>
2021-03-08 01:03:00 +0100

    Initial commit

---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = 'AIzaSyAVnoRh-v8StM9YnPHt11FsOnY6KmtCYpA';
+
+export default GOOGLE_API_KEY;
---
+++ screens/OrderDelivery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const OrderDelivery = () => {
+  function renderMap() {
+    <View></View>;
+  }
+
+  return <View style={{flex: 1}}>{renderMap()}</View>;
+};
+
+export default OrderDelivery;
